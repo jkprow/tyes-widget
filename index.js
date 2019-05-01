d851b99552ababa70779e10da57cecae51a51cbd
James Prow <jkprow@gmail.com>
2019-04-30 22:08:57 -0700

    make safari happy

--- index.js
+++ index.js
@@ -1,48 +1,67 @@
+// make safari happy
+if (!window.AudioContext && window.webkitAudioContext) {
+  const oldFunc = webkitAudioContext.prototype.decodeAudioData;
+  webkitAudioContext.prototype.decodeAudioData = function(arraybuffer) {
+    return new Promise((resolve, reject) => {
+      oldFunc.call(this, arraybuffer, resolve, reject);
+    });
+  }
+}
+
 (async function() {
   const S3_ROOT = 'https://s3.us-west-2.amazonaws.com/jkprow/';
   const FILES = ['beachboiz.wav', 'dnb.wav'];
   
-  const ctx = new AudioContext();
+  const ctx = new (window.AudioContext || window.webkitAudioContext)();
   
-  const nodeGroups = await Promise.all(
-    await FILES.map(async loop => {
-      const response = await fetch(S3_ROOT + loop);
-      const arrayBuffer = await response.arrayBuffer();
-      const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
-      const gainNode = ctx.createGain();
-      const bufferSourceNode = ctx.createBufferSource();
-      gainNode.gain.value = 0;
-      bufferSourceNode.buffer = audioBuffer;
-      bufferSourceNode.connect(gainNode);
-      gainNode.connect(ctx.destination);
-      bufferSourceNode.loop = true;
-      return {
-        gain: gainNode,
-        bufferSource: bufferSourceNode,
-      };
-    })
-  );
+  async function rest() {
+    const nodeGroups = await Promise.all(
+      await FILES.map(async loop => {
+        const response = await fetch(S3_ROOT + loop);
+        const arrayBuffer = await response.arrayBuffer();
+        const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
+        const gainNode = ctx.createGain();
+        const bufferSourceNode = ctx.createBufferSource();
+        gainNode.gain.value = 0;
+        bufferSourceNode.buffer = audioBuffer;
+        bufferSourceNode.connect(gainNode);
+        gainNode.connect(ctx.destination);
+        bufferSourceNode.loop = true;
+        return {
+          gain: gainNode,
+          bufferSource: bufferSourceNode,
+        };
+      })
+    );
   
-  const gains = nodeGroups.map(({ gain, bufferSource }) => {
-    bufferSource.start();
-    return gain;
-  });
-
-  const top = document.getElementById('top');
-  const middle = document.getElementById('middle');
-  const bottom = document.getElementById('bottom');
+    const gains = nodeGroups.map(({gain, bufferSource}) => {
+      bufferSource.start(0);
+      return gain;
+    });
   
-  top.addEventListener('click', () => {
-    console.log('Layer 1');
-    gains[0].gain.value = 1 - gains[0].gain.value;
-  });
+    const top = document.getElementById('top');
+    const middle = document.getElementById('middle');
+    const bottom = document.getElementById('bottom');
   
-  middle.addEventListener('click', () => {
-    console.log('Layer 2');
-  });
+    top.addEventListener('click', () => {
+      console.log('Layer 1');
+      gains[0].gain.value = 1 - gains[0].gain.value;
+    });
   
-  bottom.addEventListener('click', () => {
-    console.log('Layer 3');
-    gains[1].gain.value = 1 - gains[0].gain.value;
-  });
+    middle.addEventListener('click', () => {
+      console.log('Layer 2');
+    });
+  
+    bottom.addEventListener('click', () => {
+      console.log('Layer 3');
+      gains[1].gain.value = 1 - gains[1].gain.value;
+    });
+  }
+  
+  if (ctx.state !== 'suspended') return;
+  const b = document.body;
+  const events = ['touchstart','touchend', 'mousedown','keydown'];
+  events.forEach(e => b.addEventListener(e, unlock, false));
+  function unlock() { ctx.resume().then(clean).then(rest); }
+  function clean() { events.forEach(e => b.removeEventListener(e, unlock)) };
 })();
