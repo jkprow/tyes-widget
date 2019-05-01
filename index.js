98d34a2dcb08e1c29d617fbc9305dd3fa9c36bc7
James Prow <jkprow@gmail.com>
2019-04-30 21:12:34 -0700

    add basic looping

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-(function() {
+(async function() {
+  const S3_ROOT = 'https://s3.us-west-2.amazonaws.com/jkprow/';
+  const FILES = ['beachboiz.wav', 'dnb.wav'];
+  
+  const ctx = new AudioContext();
+  
+  const nodeGroups = await Promise.all(
+    await FILES.map(async loop => {
+      const response = await fetch(S3_ROOT + loop);
+      const arrayBuffer = await response.arrayBuffer();
+      const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
+      const gainNode = ctx.createGain();
+      const bufferSourceNode = ctx.createBufferSource();
+      gainNode.gain.value = 0;
+      bufferSourceNode.buffer = audioBuffer;
+      bufferSourceNode.connect(gainNode);
+      gainNode.connect(ctx.destination);
+      bufferSourceNode.loop = true;
+      return {
+        gain: gainNode,
+        bufferSource: bufferSourceNode,
+      };
+    })
+  );
+  
+  const gains = nodeGroups.map(({ gain, bufferSource }) => {
+    bufferSource.start();
+    return gain;
+  });
+
   const top = document.getElementById('top');
   const middle = document.getElementById('middle');
   const bottom = document.getElementById('bottom');
   
   top.addEventListener('click', () => {
     console.log('Layer 1');
+    gains[0].gain.value = 1 - gains[0].gain.value;
   });
   
   middle.addEventListener('click', () => {
@@ -13,5 +43,6 @@
   
   bottom.addEventListener('click', () => {
     console.log('Layer 3');
+    gains[1].gain.value = 1 - gains[0].gain.value;
   });
 })();
