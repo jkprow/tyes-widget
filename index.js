8ae995f8e7742833b2c3f2ade55906bbe77617df
James Prow <jkprow@gmail.com>
2019-05-03 21:11:43 -0700

    improve init; add gain ramping; reorg

--- index.js
+++ index.js
@@ -9,59 +9,73 @@ if (!window.AudioContext && window.webkitAudioContext) {
 }
 
 (async function() {
+  // Globals
   const S3_ROOT = 'https://s3.us-west-2.amazonaws.com/jkprow/';
   const FILES = ['beachboiz.wav', 'dnb.wav'];
-  
   const ctx = new (window.AudioContext || window.webkitAudioContext)();
   
-  async function rest() {
-    const nodeGroups = await Promise.all(
-      await FILES.map(async loop => {
-        const response = await fetch(S3_ROOT + loop);
-        const arrayBuffer = await response.arrayBuffer();
-        const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
-        const gainNode = ctx.createGain();
-        const bufferSourceNode = ctx.createBufferSource();
-        gainNode.gain.value = 0;
-        bufferSourceNode.buffer = audioBuffer;
-        bufferSourceNode.connect(gainNode);
-        gainNode.connect(ctx.destination);
-        bufferSourceNode.loop = true;
-        return {
-          gain: gainNode,
-          bufferSource: bufferSourceNode,
-        };
-      })
+  // Wait for user gesture to init AudioContext
+  if (ctx.state === 'suspended') {
+    const events = ['touchstart', 'touchend', 'mousedown', 'keydown'];
+    await Promise.race(
+      events.map(event => new Promise(resolve => {
+        document.body.addEventListener(event, resolve, false);
+      }))
     );
-  
-    const gains = nodeGroups.map(({gain, bufferSource}) => {
-      bufferSource.start(0);
-      return gain;
-    });
-  
-    const top = document.getElementById('top');
-    const middle = document.getElementById('middle');
-    const bottom = document.getElementById('bottom');
-  
-    top.addEventListener('click', () => {
-      console.log('Layer 1');
-      gains[0].gain.value = 1 - gains[0].gain.value;
-    });
-  
-    middle.addEventListener('click', () => {
-      console.log('Layer 2');
-    });
-  
-    bottom.addEventListener('click', () => {
-      console.log('Layer 3');
-      gains[1].gain.value = 1 - gains[1].gain.value;
-    });
+    events.forEach(e => document.body.removeEventListener(e, this));
+    await ctx.resume();
   }
   
-  if (ctx.state !== 'suspended') return;
-  const b = document.body;
-  const events = ['touchstart','touchend', 'mousedown','keydown'];
-  events.forEach(e => b.addEventListener(e, unlock, false));
-  function unlock() { ctx.resume().then(clean).then(rest); }
-  function clean() { events.forEach(e => b.removeEventListener(e, unlock)) };
+  // Fetch the loop files and set up their audio graph
+  const gainBufferPairs = await Promise.all(
+    await FILES.map(async loop => {
+      const response = await fetch(S3_ROOT + loop);
+      const arrayBuffer = await response.arrayBuffer();
+      const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
+      const gainNode = ctx.createGain();
+      const bufferSourceNode = ctx.createBufferSource();
+      gainNode.gain.value = 0;
+      bufferSourceNode.buffer = audioBuffer;
+      bufferSourceNode.connect(gainNode);
+      gainNode.connect(ctx.destination);
+      bufferSourceNode.loop = true;
+      return {
+        gain: gainNode,
+        bufferSource: bufferSourceNode,
+      };
+    })
+  );
+
+  // Once both loops are ready, kick them off
+  const gains = gainBufferPairs.map(({gain, bufferSource}) => {
+    bufferSource.start(0);
+    return gain;
+  });
+  
+  /**
+   * Given a GainNode, toggle its gain from 0% to 100%
+   * over a 1s ramp
+   * @param {GainNode} gainNode
+   */
+  function toggleGain(gainNode) {
+    const gain = gainNode.gain;
+    gain.linearRampToValueAtTime(1 - gain.value, ctx.currentTime + 1);
+  }
+  
+  // Set up event listeners
+  const top = document.getElementById('top');
+  const middle = document.getElementById('middle');
+  const bottom = document.getElementById('bottom');
+
+  top.addEventListener('click', () => {
+    toggleGain(gains[0]);
+  });
+
+  middle.addEventListener('click', () => {
+    console.log('Layer 2');
+  });
+
+  bottom.addEventListener('click', () => {
+    toggleGain(gains[1]);
+  });
 })();
