30e5e3dcc415fbb31804c4da8cb2a1438365a8aa
James Prow <jkprow@gmail.com>
2019-05-15 11:09:32 -0700

    image swapping

--- index.js
+++ index.js
@@ -34,6 +34,19 @@ if (!window.AudioContext && window.webkitAudioContext) {
   // Globals
   const S3_ROOT = 'https://s3.us-west-2.amazonaws.com/jkprow/';
   const FILES = ['beachboiz.wav', 'dnb.wav'];
+  const IMAGES = [[
+    // Piano
+    { url: 'img/piano/off.png' },
+    { url: 'img/piano/on.gif' },
+  ], [
+    { url: 'img/mixer/off.png' },
+    { url: 'img/mixer/toOn.gif', isTransition: true },
+    { url: 'img/mixer/on.png' },
+    { url: 'img/mixer/toOff.gif', isTransition: true },
+  ], [
+    { url: 'img/trees/off.png' },
+    { url: 'img/trees/on.gif' },
+  ]];
   const ctx = new (window.AudioContext || window.webkitAudioContext)();
   
   // Wait for user gesture to init AudioContext
@@ -63,7 +76,12 @@ if (!window.AudioContext && window.webkitAudioContext) {
   // Fetch the loop files and set up their audio graph
   const loopNodes = await Promise.all(
     await FILES.map(async (loop, idx) => {
-      const response = await fetch(S3_ROOT + loop);
+      let response;
+      try {
+        response = await fetch(S3_ROOT + loop);
+      } catch (e) {
+        return {};
+      }
       const arrayBuffer = await response.arrayBuffer();
       const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
       const loopGain = ctx.createGain();
@@ -83,6 +101,10 @@ if (!window.AudioContext && window.webkitAudioContext) {
 
   // Once both loops are ready, kick them off
   const gains = loopNodes.map(({ loopGain, loopBufferSource }) => {
+    if (!loopGain) {
+      return null;
+    }
+    
     loopBufferSource.start(0);
     return loopGain;
   });
@@ -93,25 +115,55 @@ if (!window.AudioContext && window.webkitAudioContext) {
    * @param {GainNode} gainNode
    */
   function toggleGain(gainNode) {
+    if (!gainNode) {
+      return;
+    }
+    
     const gain = gainNode.gain;
     gain.linearRampToValueAtTime(1 - gain.value, ctx.currentTime + 1);
+  }
+  
+  function toggleImg(imgs, el) {
+    if (el.imgSrcIndex === imgs.length - 1) {
+      el.imgSrcIndex = 0;
+    } else {
+      el.imgSrcIndex++;
+    }
+    const nextImg = imgs[el.imgSrcIndex];
+    el.src = nextImg.url;
+    if (nextImg.isTransition) {
+      setTimeout(() => toggleImg(imgs, el), 1500);
+    }
   }
   
   // Set up event listeners
   const top = document.getElementById('top');
   const middle = document.getElementById('middle');
   const bottom = document.getElementById('bottom');
-
-  top.addEventListener('click', () => {
+  
+  top.imgSrcIndex = 0;
+  middle.imgSrcIndex = 0;
+  bottom.imgSrcIndex = 0;
+  
+  window.toggleTop = () => {
     toggleGain(gains[0]);
-  });
-
-  middle.addEventListener('click', () => {
+    toggleImg(IMAGES[0], top)
+  };
+  
+  window.toggleMiddle = () => {
     toggleGain(wetGain);
     toggleGain(dryGain);
-  });
-
-  bottom.addEventListener('click', () => {
+    toggleImg(IMAGES[1], middle)
+  };
+  
+  window.toggleBottom = () => {
     toggleGain(gains[1]);
-  });
+    toggleImg(IMAGES[2], bottom);
+  };
+
+  top.addEventListener('click', toggleTop);
+
+  middle.addEventListener('click', toggleMiddle);
+
+  bottom.addEventListener('click', toggleBottom);
 })();
