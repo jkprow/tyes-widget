f8351e9423dc9d1401ebaf8792a83b4b97be6988
James Prow <jkprow@gmail.com>
2019-05-15 11:51:12 -0700

    refactor

--- index.js
+++ index.js
@@ -9,6 +9,32 @@ if (!window.AudioContext && window.webkitAudioContext) {
 }
 
 (async function() {
+  const S3_ROOT = 'https://s3.us-west-2.amazonaws.com/jkprow/';
+  const ctx = new (window.AudioContext || window.webkitAudioContext)();
+  
+  // Init Context
+  if (ctx.state === 'suspended') {
+    const events = ['touchstart', 'touchend', 'mousedown', 'keydown'];
+    await Promise.race(
+      events.map(event => new Promise(resolve => {
+        document.body.addEventListener(event, resolve, false);
+      }))
+    );
+    events.forEach(e => document.body.removeEventListener(e, this));
+    await ctx.resume();
+  }
+  
+  // Set up gains
+  const dryGain = ctx.createGain();
+  dryGain.gain.value = 1;
+  dryGain.connect(ctx.destination);
+  
+  const wetGain = ctx.createGain();
+  wetGain.gain.value = 0;
+  wetGain.connect(ctx.destination);
+  
+  // Set up reverb
+  
   function makeReverb(seconds = 1, decay = 3) {
     const reverb = ctx.createConvolver();
     const rate = ctx.sampleRate;
@@ -27,143 +53,117 @@ if (!window.AudioContext && window.webkitAudioContext) {
     }
     
     reverb.buffer = impulse;
-    
     return reverb;
   }
-  
-  // Globals
-  const S3_ROOT = 'https://s3.us-west-2.amazonaws.com/jkprow/';
-  const FILES = ['beachboiz.wav', 'dnb.wav'];
-  const IMAGES = [[
-    // Piano
-    { url: 'img/piano/off.png' },
-    { url: 'img/piano/on.gif' },
-  ], [
-    { url: 'img/mixer/off.png' },
-    { url: 'img/mixer/toOn.gif', isTransition: true },
-    { url: 'img/mixer/on.png' },
-    { url: 'img/mixer/toOff.gif', isTransition: true },
-  ], [
-    { url: 'img/trees/off.png' },
-    { url: 'img/trees/on.gif' },
-  ]];
-  const ctx = new (window.AudioContext || window.webkitAudioContext)();
-  
-  // Wait for user gesture to init AudioContext
-  if (ctx.state === 'suspended') {
-    const events = ['touchstart', 'touchend', 'mousedown', 'keydown'];
-    await Promise.race(
-      events.map(event => new Promise(resolve => {
-        document.body.addEventListener(event, resolve, false);
-      }))
-    );
-    events.forEach(e => document.body.removeEventListener(e, this));
-    await ctx.resume();
-  }
-  
-  // Set up base audio graph
-  const dryGain = ctx.createGain();
-  dryGain.gain.value = 1;
-  dryGain.connect(ctx.destination);
-  
-  const wetGain = ctx.createGain();
-  wetGain.gain.value = 0;
-  wetGain.connect(ctx.destination);
   
   const reverb = makeReverb();
   reverb.connect(wetGain);
   
-  // Fetch the loop files and set up their audio graph
-  const loopNodes = await Promise.all(
-    await FILES.map(async (loop, idx) => {
-      let response;
-      try {
-        response = await fetch(S3_ROOT + loop);
-      } catch (e) {
-        return {};
-      }
-      const arrayBuffer = await response.arrayBuffer();
-      const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
-      const loopGain = ctx.createGain();
-      const loopBufferSource = ctx.createBufferSource();
-      loopGain.gain.value = 0;
-      loopBufferSource.buffer = audioBuffer;
-      loopBufferSource.connect(loopGain);
-      loopGain.connect(reverb);
-      loopGain.connect(dryGain);
-      loopBufferSource.loop = true;
-      return {
-        loopGain,
-        loopBufferSource,
-      };
-    })
-  );
-
-  // Once both loops are ready, kick them off
-  const gains = loopNodes.map(({ loopGain, loopBufferSource }) => {
-    if (!loopGain) {
-      return null;
+  class Layer {
+    static toggleGain(gain) {
+      gain.linearRampToValueAtTime(1 - gain.value, ctx.currentTime + 1);
     }
     
-    loopBufferSource.start(0);
-    return loopGain;
+    constructor(args) {
+      this.el = document.getElementById(args.id);
+      this.el.addEventListener('click', () => this.toggle());
+  
+      this.audioURL = args.audioURL;
+      this.loopGain = null;
+      this.loopBufferSource = null;
+  
+      this.imageURLs = args.imageURLs;
+      this.imageSourceIndex = 0;
+    }
+    
+    async initAudio() {
+      this.loopBufferSource = ctx.createBufferSource();
+      this.loopBufferSource.loop = true;
+      
+      try {
+        const response = await fetch(this.audioURL);
+        const arrayBuffer = await response.arrayBuffer();
+        this.loopBufferSource = await ctx.decodeAudioData(arrayBuffer);
+      } catch (e) {
+        console.log("Couldn't fetch audio data.");
+      }
+      
+      this.loopGain = ctx.createGain();
+      this.loopGain.gain.value = 0;
+  
+      this.loopBufferSource.connect(this.loopGain);
+      this.loopGain.connect(reverb);
+      this.loopGain.connect(dryGain);
+    }
+    
+    startLoop() {
+      this.loopBufferSource.start(0);
+    }
+    
+    toggle() {
+      console.log('toggle');
+      this.toggleImage();
+      this.toggleAudio();
+    }
+    
+    toggleAudio() {
+      Layer.toggleGain(this.loopGain.gain);
+    }
+    
+    toggleImage() {
+      if (this.imageSourceIndex === this.imageURLs.length - 1) {
+        this.imageSourceIndex = 0;
+      } else {
+        this.imageSourceIndex++;
+      }
+      
+      const nextImage = this.imageURLs[this.imageSourceIndex];
+      this.el.src = nextImage.url;
+      
+      if (nextImage.isTransition) {
+        setTimeout(() => this.toggleImage(), 1500);
+      }
+    }
+  }
+  
+  const keyboardLayer = new Layer({
+    id: 'top',
+    audioURL: S3_ROOT + 'beachboiz.wav',
+    imageURLs: [
+      { url: 'img/piano/off.png' },
+      { url: 'img/piano/on.gif' },
+    ],
   });
   
-  /**
-   * Given a GainNode, toggle its gain from 0% to 100%
-   * over a 1s ramp
-   * @param {GainNode} gainNode
-   */
-  function toggleGain(gainNode) {
-    if (!gainNode) {
-      return;
-    }
-    
-    const gain = gainNode.gain;
-    gain.linearRampToValueAtTime(1 - gain.value, ctx.currentTime + 1);
-  }
+  const treeLayer = new Layer({
+    id: 'bottom',
+    audioURL: S3_ROOT + 'dnb.wav',
+    imageURLs: [
+      { url: 'img/trees/off.png' },
+      { url: 'img/trees/on.gif' },
+    ],
+  });
   
-  function toggleImg(imgs, el) {
-    if (el.imgSrcIndex === imgs.length - 1) {
-      el.imgSrcIndex = 0;
-    } else {
-      el.imgSrcIndex++;
-    }
-    const nextImg = imgs[el.imgSrcIndex];
-    el.src = nextImg.url;
-    if (nextImg.isTransition) {
-      setTimeout(() => toggleImg(imgs, el), 1500);
-    }
-  }
+  const mixerLayer = new Layer({
+    id: 'middle',
+    imageURLs: [
+      { url: 'img/mixer/off.png' },
+      { url: 'img/mixer/toOn.gif', isTransition: true },
+      { url: 'img/mixer/on.png' },
+      { url: 'img/mixer/toOff.gif', isTransition: true },
+    ],
+  });
   
-  // Set up event listeners
-  const top = document.getElementById('top');
-  const middle = document.getElementById('middle');
-  const bottom = document.getElementById('bottom');
-  
-  top.imgSrcIndex = 0;
-  middle.imgSrcIndex = 0;
-  bottom.imgSrcIndex = 0;
-  
-  window.toggleTop = () => {
-    toggleGain(gains[0]);
-    toggleImg(IMAGES[0], top)
-  };
-  
-  window.toggleMiddle = () => {
-    toggleGain(wetGain);
-    toggleGain(dryGain);
-    toggleImg(IMAGES[1], middle)
-  };
-  
-  window.toggleBottom = () => {
-    toggleGain(gains[1]);
-    toggleImg(IMAGES[2], bottom);
+  mixerLayer.toggleAudio = function() {
+    Layer.toggleGain(wetGain.gain);
+    Layer.toggleGain(dryGain.gain);
   };
 
-  top.addEventListener('click', toggleTop);
+  await Promise.all([
+    keyboardLayer.initAudio(),
+    treeLayer.initAudio(),
+  ]);
 
-  middle.addEventListener('click', toggleMiddle);
-
-  bottom.addEventListener('click', toggleBottom);
+  keyboardLayer.startLoop();
+  treeLayer.startLoop();
 })();
